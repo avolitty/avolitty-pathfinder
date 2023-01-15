0ec4161dc6aadcdbed15db71bb50e64b6bf118de
William Parsons <avolitty@gmail.com>
2023-01-14 16:23:25 -0800

    Promoting destination height and width to unsigned long int.

--- src/avolitty-pathfinder.c
+++ src/avolitty-pathfinder.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 
-static void AvolittyPathfinderA(unsigned long int *a, unsigned long int *b, unsigned long int *c, unsigned long int *d, signed long int *e, unsigned long int f, unsigned long int g, unsigned long int h, unsigned long int i, unsigned long int j, unsigned char *k, unsigned char *l, unsigned char m, unsigned char n) {
+static void AvolittyPathfinderA(unsigned long int *a, unsigned long int *b, unsigned long int *c, unsigned long int *d, signed long int *e, unsigned long int f, unsigned long int g, unsigned long int h, unsigned long int i, unsigned long int j, unsigned long int k, unsigned long int l, unsigned char *m, unsigned char *n) {
 	/*
 		a -> [array] cur pos
 		b -> [array] cur pos heights
 		c -> [array] cur pos widths
-		d -> [array] traversal direction increments
-		e -> [integer] length of cur pos array
-		f -> [integer] grid height
-		g -> [integer] grid width
-		h -> [integer] src pos
-		i -> [integer] length of grid
-		j -> [integer] dst pos
-		k -> [array] grid
-		l -> pointer [integer] traversal status 0|1
-		m -> [integer] dst height
-		n -> [integer] dst width
+		d -> [integer] length of cur pos array
+		e -> [array] traversal direction increments
+		f -> [integer] dst height
+                g -> [integer] dst width
+		h -> [integer] grid height
+		i -> [integer] grid width
+		j -> [integer] src pos
+		k -> [integer] length of grid
+		l -> [integer] dst pos
+		m -> [array] grid
+		n -> pointer [integer] traversal status 0|1
 	*/
 	unsigned long int o = *d;
+	unsigned long int p;
+	unsigned char q;
 
 	while (o != 0UL) {
 		o--;
+		q = 0U;
 		/* .. */
 	}
 
-	*l = 1U;
+	*n = 1U;
 	return;
 }
 
@@ -34,11 +37,11 @@ void AvolittyPathfinder(unsigned long int *a, unsigned long int *b, unsigned lon
 	unsigned long int j = i;
 	unsigned long int k = 1UL;
 	unsigned long int l;
-	unsigned long int *m = &k;
-	unsigned char n = 0U;
-	unsigned char o;
-	unsigned char p;
-	unsigned char *q = &n;
+	unsigned long int m;
+	unsigned long int n;
+	unsigned long int *o = &k;
+	unsigned char p = 0U;
+	unsigned char *q = &p;
 
 	while (i != 0UL) {
 		i--;
@@ -46,6 +49,9 @@ void AvolittyPathfinder(unsigned long int *a, unsigned long int *b, unsigned lon
 	}
 
 	i = h[1U];
+	a[0] = i;
+	b[0] = i / d;
+	c[0] = i - ((i / e) * 10UL);
 	l = h[2U];
 	f[l] = 3U;
 	h[0U] = h[8U] - 1L;
@@ -54,28 +60,28 @@ void AvolittyPathfinder(unsigned long int *a, unsigned long int *b, unsigned lon
 	h[3U] = 0L;
 	h[4U] = h[8U] + 1L;
 	h[5U] = h[9U] + 1L;
-	o = (l / d) + 1U;
-	p = (l - ((l / e) * 10U)) + 1U;
+	m = l / d;
+	n = l - ((l / e) * 10UL);
 
-	while (n == 0U) {
+	while (p == 0U) {
 		/*
 			a -> [array] cur pos
 			b -> [array] cur pos heights
-			c -> [array] widths for cur pos
-			m -> [integer] length of cur pos array
+			c -> [array] cur pos widths
+			o -> [integer] length of cur pos array
 			h -> [array] traversal direction increments
+			m -> [integer] dst height
+                        n -> [integer] dst width
 			d -> [integer] grid height
 			e -> [integer] grid width
 			i -> [integer] src pos
 			j -> [integer] length of grid
-			k -> [integer] dst pos
+			l -> [integer] dst pos
 			f -> [array] grid
 			q -> pointer [integer] traversal status 0|1
-			o -> [integer] dst height
-			p -> [integer] dst width
 		*/
 
-		AvolittyPathfinderA(a, b, c, m, h, d, e, i, j, l, f, q, o, p);
+		AvolittyPathfinderA(a, b, c, o, h, m, n, d, e, i, j, l, f, q);
 	}
 
 	return;
