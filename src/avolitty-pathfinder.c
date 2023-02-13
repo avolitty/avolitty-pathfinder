6630251fc81be1d3efe4c4aeb0e6960b320f2e5b
William Parsons <avolitty@gmail.com>
2023-02-12 19:24:47 -0800

    Adding obstacle traversal for left traversal.

--- src/avolitty-pathfinder.c
+++ src/avolitty-pathfinder.c
@@ -160,10 +160,93 @@ static void AvolittyPathfinderA(unsigned long int *a, unsigned long int *b, unsi
 		if (x == 0U) {
 			if ((w == 2U) || (w == 8U)) {
 				if (w == 2U) {
-					/* [left]
-						[up [left]   + down [left]]
-						[right [up]  + right [down]] */
-					printf("left\n");
+					printf("obstacle traversal steps after collision from [left] non-obstacle traversal\n");
+					t = (o - s);
+
+					if ((m[t] == 3U) && (q != 0UL)) {
+						while ((m[t] == 3U) && (q != 0UL) && (h != r)) {
+							t++;
+
+							if (m[t] == 3U) {
+								r++;
+							} else {
+								q--;
+								t -= (s + 1UL);
+							}
+						}
+
+						x = ((unsigned char) ((q == 0UL) || (h == r)));
+					} else {
+						t = (o - 1UL);
+
+                                                while ((m[t] == 3U) && (q != 0UL) && (r != 0UL)) {
+							t -= s;
+
+							if (m[t] == 3U) {
+								q--;
+							} else {
+								r--;
+								t += (s - 1UL);
+							}
+						}
+
+						x = ((unsigned char) ((q == 0UL) || (r == 0UL)));
+					}
+
+					if (x == 0U) {
+						*d = t;
+						*e = q;
+						*f = r;
+						AvolittyPathfinderA(a, b, c, d, e, f, g, h, i, j, k, l, m, (unsigned char) 1U);
+					}
+
+					q = u;
+					r = v;
+					t = (o + s);
+
+					if ((m[t] == 3U) && (g != q)) {
+						while ((m[t] == 3U) && (g != q) && (h != r)) {
+							t++;
+
+							if (m[t] == 3U) {
+								r++;
+							} else {
+								q++;
+								t += (s - 1UL);
+							}
+						}
+
+						x = ((unsigned char) ((g == q) || (h == r)));
+					} else {
+						t = (o - 1UL);
+
+						while ((m[t] == 3U) && (g != q) && (r != 0UL)) {
+							t += s;
+
+							if (m[t] == 3U) {
+								q++;
+							} else {
+								r--;
+								t -= (s + 1UL);
+							}
+						}
+
+						x = ((unsigned char) ((g == q) || (r == 0UL)));
+					}
+
+					if (x == 0U) {
+						*d = t;
+						*e = q;
+						*f = r;
+						AvolittyPathfinderA(a, b, c, d, e, f, g, h, i, j, k, l, m, (unsigned char) 1U);
+
+						if (n == 0U) {
+							printf("Second split dst from obstacle traversal after obstacle collision: %lu\n", *d);
+							printf("Original src: %lu\n", p);
+							AvolittyPathfinderA(a, b, c, d, e, f, g, h, i, j, k, l, m, (unsigned char) 1U);
+						}
+					}
+
 					return;
 				} else {
 					printf("obstacle traversal steps after collision from [up] non-obstacle traversal\n");
