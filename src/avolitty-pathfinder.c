a943e265a85fa6a2cf1aa1b1dcf447ed374bdb6a
William Parsons <avolitty@gmail.com>
2023-02-18 18:47:36 -0800

    Adding traversal direction increment calculation to AvolittyPathfinder().

--- src/avolitty-pathfinder.c
+++ src/avolitty-pathfinder.c
@@ -31,18 +31,23 @@ void AvolittyPathfinder(unsigned long int a, unsigned long int b, unsigned char 
 	unsigned char n;
 	unsigned char o;
 	unsigned char p;
+	unsigned char q;
+	unsigned char r;
 	e = (a * b);
 	f = e;
 	g = ((unsigned long int) 0UL);
-	n = ((unsigned char) 3U);
-	o = ((unsigned char) 2U);
-	p = ((unsigned char) 1U);
+	n = ((unsigned char) 4U);
+	o = ((unsigned char) 3U);
+	p = ((unsigned char) 2U);
+	q = ((unsigned char) 1U);
+	r = ((unsigned char) 0U);
 
 	while (e != g) {
 		e--;
 		h = c[e];
+		d[h] = e;
 
-		if (h == n) {
+		if (h == o) {
 			h = (e / b);
 			i = (e % b);
 			j = e;
@@ -66,26 +71,41 @@ void AvolittyPathfinder(unsigned long int a, unsigned long int b, unsigned char 
 				m--;
 			}
 
-			if (((c[j] != n) || (c[l] != n)) && ((c[k] != n) || (c[m] != n))) {
-				if (c[j] != n) {
-					c[j] = o;
+			if (((c[j] != o) || (c[l] != o)) && ((c[k] != o) || (c[m] != o))) {
+				if (c[j] != o) {
+					c[j] = n;
 				}
 
-				if (c[k] != n) {
-					c[k] = o;
+				if (c[k] != o) {
+					c[k] = n;
 				}
 
-				if (c[l] != n) {
-					c[l] = o;
+				if (c[l] != o) {
+					c[l] = n;
 				}
 
-				if (c[m] != n) {
-					c[m] = o;
+				if (c[m] != o) {
+					c[m] = n;
 				}
 			}
 		}
 	}
 
+	e = d[p];
+	c[e] = n;
+	h = d[q];
+	c[h] = n;
+	g++;
+	d[n] = d[q];
+	d[o] = a;
+	d[p] = g;
+	d[q] = (b - g);
+	d[r] = (b + g);
+	d[((unsigned char) 5U)] = d[r];
+	d[((unsigned char) 6U)] = g;
+	d[((unsigned char) 7U)] = a;
+	d[((unsigned char) 8U)] = b;
+	d[((unsigned char) 9U)] = b;
 	/* debug grid output */
 	e = f;
 	g = 0UL;
@@ -99,6 +119,19 @@ void AvolittyPathfinder(unsigned long int a, unsigned long int b, unsigned char 
 		}
 	}
 
+	/*
+                * a -> [array] traversal direction increments
+                * b -> [integer] cur pos
+                * c -> [integer] cur pos height
+                * d -> [integer] cur pos width
+                e -> [integer] grid height
+                f -> [integer] grid width
+                g -> [integer] grid length
+                h -> [integer] dst pos
+                i -> [integer] dst height
+                j -> [integer] dst width
+                * k -> [array] grid
+        */
 	AvolittyPathfinderA();
 	printf("\n");
 	/* .. */
